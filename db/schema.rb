5780d4e97f798c2612492b3edd2aee72e746c51d
Adam Sweeney <adam.a.sweeney@gmail.com>
2017-08-12 08:23:01 -0400

    Remove username column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170808213524) do
+ActiveRecord::Schema.define(version: 20170812112730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 20170808213524) do
 
   create_table "users", force: :cascade do |t|
     t.string "access_token", default: "", null: false
-    t.string "username", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
