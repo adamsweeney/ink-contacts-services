17a5939ab83e5a8a2085c6d04942de81aa181781
Adam Sweeney <adam.a.sweeney@gmail.com>
2017-08-14 08:58:31 -0400

    Make default_url the thumbnail

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ApplicationRecord
 	belongs_to :user
 
-	has_attached_file :avatar, styles: { medium: "300x300", thumb: "100x100" }
+	has_attached_file :avatar, styles: { medium: "300x300", thumb: "100x100" }, default_url: "#{Paperclip::Attachment.default_options[:fog_host]}/default-thumbnail.jpg"
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
